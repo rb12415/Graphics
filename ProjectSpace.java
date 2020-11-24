db5d85c0212971ca00569d6e7ebce94bcd56299b
Crom89 <ob4cl88@aol.com>
2020-11-23 20:54:24 -0500

    Add files via upload

--- ProjectSpace.java
+++ ProjectSpace.java
@@ -22,16 +22,16 @@ public class ProjectSpace extends Applet {
     int orbitTimeMs = 4000;
     int rotationTimeMs = 1500;
 
-    int earthRotationTimeMs = 190;
+    int earthRotationTimeMs = 198;
     int earthOrbitTimeMs = 200;
 
     int moonRotationTimeMs = 48;
     int moonOrbitTimeMs = 50;
 
-    int mercuryRotationTimeMs = 90;
+    int mercuryRotationTimeMs = 99;
     int mercuryOrbitTimeMs = 100;
 
-    int venusRotationTimeMs = 140;
+    int venusRotationTimeMs = 149;
     int venusOrbitTimeMs = 150;
 
     int marsRotationTimeMs = 240;
@@ -59,8 +59,7 @@ public class ProjectSpace extends Applet {
     private static BoundingSphere boundingSphere = new BoundingSphere(new Point3d(0.0,0.0,0.0), Double.MAX_VALUE);
 
     public static void main(String[] args){
-        new MainFrame(new ProjectSpace(), 800,600);
-
+        new MainFrame(new ProjectSpace(), 1000,800);
 
 
     }
@@ -114,16 +113,18 @@ public class ProjectSpace extends Applet {
         //.375f for the size of moon
 
         Material mat = new Material();
-        mat.setEmissiveColor(255, 0, 0);
+        mat.setLightingEnable(true);
+        //mat.setEmissiveColor(new Color3f(.937f,0.556f,0.219f));
+        mat.setEmissiveColor(new Color3f(1.0f,0.898f,0.8f));
         Appearance app = new Appearance();
-        Color3f color = new Color3f(Color.white);
+        /*Color3f color = new Color3f(Color.white);
         Color3f black = new Color3f(0.0f, 0.0f, 0.0f);
-        Color3f white = new Color3f(1.0f, 1.0f, 1.0f);
-        mat.setSpecularColor(new Color3f(Color.red));
-        mat.setShininess(100f);
+        Color3f white = new Color3f(1.0f, 1.0f, 1.0f);*/
+        //mat.setSpecularColor(new Color3f(239,142,56));
+        mat.setShininess(50.0f);
         app.setMaterial(mat);
         sun.setAppearance(app);
-        mercury.setAppearance(app);
+        /*mercury.setAppearance(app);
         venus.setAppearance(app);
         earth.setAppearance(app);
         mars.setAppearance(app);
@@ -132,30 +133,12 @@ public class ProjectSpace extends Applet {
         uranus.setAppearance(app);
         neptune.setAppearance(app);
         pluto.setAppearance(app);
-        moon.setAppearance(app);
+        moon.setAppearance(app);*/
 
-        //place each sphere in its transform group
         Transform3D tr = new Transform3D();
-        // tr.setScale(1.5);
-        //TransformGroup tg = new TransformGroup(tr);
-        // spin.addChild(tg);
-        //tg.addChild(sun);
-        //tr.setIdentity();
-        //tr.setTranslation(new Vector3f(0f,0f,0f));
-
-
-        // TransformGroup tgSun = new TransformGroup(tr);//coordinates 0f,0f,0f
-        // tgSolar.addChild(tgSun);
-        // tgSun.addChild(sun);
-        //  tr.setScale(1.5);//set the scale of the sun
-        // tr.setIdentity();//i'm not too sure
-        //tr.setTranslation(new Vector3f(0f,0f,0f));
-
-        //tr.setScale(.002); was orignally .2
-        // tr.setIdentity();
 
         //Sun
-        tr.setTranslation(new Vector3f(0,.0f,.0f)); //distance to center
+        tr.setTranslation(new Vector3f(0,0f,0f)); //distance to center
         TransformGroup tgSun = new TransformGroup(tr);//-.2f,.15f,.15f
         TransformGroup tgSunRotat =createRotationTransformGroup(sunRotationTimeMs);//rotation
         TransformGroup tgSunOrbit = createOrbitTransformGroup(sunOrbitTimesMs);
@@ -165,7 +148,7 @@ public class ProjectSpace extends Applet {
         root.addChild(tgSunOrbit);
 
         //Mercury
-        tr.setTranslation(new Vector3f(75f,.0f,.0f)); //distance to center
+        tr.setTranslation(new Vector3f(75f,0f,0f)); //distance to center
         TransformGroup tgMercury = new TransformGroup(tr);//-.2f,.15f,.15f
         TransformGroup tgMercuryRotat =createRotationTransformGroup(mercuryRotationTimeMs);//rotation
         TransformGroup tgMercuryOrbit = createOrbitTransformGroup(mercuryOrbitTimeMs);
@@ -175,7 +158,7 @@ public class ProjectSpace extends Applet {
         root.addChild(tgMercuryOrbit);
 
         //Venus
-        tr.setTranslation(new Vector3f(85f,.0f,.0f)); //distance to center
+        tr.setTranslation(new Vector3f(85f,0f,0f)); //distance to center
         TransformGroup tgVenus = new TransformGroup(tr);//-.2f,.15f,.15f
         TransformGroup tgVenusRotat =createRotationTransformGroup(venusRotationTimeMs);//rotation
         TransformGroup tgVenusOrbit = createOrbitTransformGroup(venusOrbitTimeMs);
@@ -185,7 +168,7 @@ public class ProjectSpace extends Applet {
         root.addChild(tgVenusOrbit);
 
         //Earth
-        tr.setTranslation(new Vector3f(95f,.0f,.0f)); //distance to center
+        tr.setTranslation(new Vector3f(95f,0f,0f)); //distance to center
         TransformGroup tgEarth = new TransformGroup(tr);//-.2f,.15f,.15f
         TransformGroup tgEarthRotat =createRotationTransformGroup(earthRotationTimeMs);//rotation
         TransformGroup tgEarthOrbit = createOrbitTransformGroup(earthOrbitTimeMs);
@@ -195,7 +178,7 @@ public class ProjectSpace extends Applet {
         root.addChild(tgEarthOrbit);
 
         //Moon
-        tr.setTranslation(new Vector3f(5f,.0f,.0f)); //distance to center
+        tr.setTranslation(new Vector3f(5f,0f,0f)); //distance to center
         TransformGroup tgMoon = new TransformGroup(tr);//-.2f,.15f,.15f
         TransformGroup tgMoonRotat =createRotationTransformGroup(moonRotationTimeMs);//rotation
         TransformGroup tgMoonOrbit = createOrbitTransformGroup(moonOrbitTimeMs);
@@ -205,7 +188,7 @@ public class ProjectSpace extends Applet {
         tgEarth.addChild(tgMoonOrbit);
 
         //Mars
-        tr.setTranslation(new Vector3f(105f,.0f,.0f)); //distance to center
+        tr.setTranslation(new Vector3f(105f,0f,0f)); //distance to center
         TransformGroup tgMars = new TransformGroup(tr);//-.2f,.15f,.15f
         TransformGroup tgMarsRotat =createRotationTransformGroup(marsRotationTimeMs);//rotation
         TransformGroup tgMarsOrbit = createOrbitTransformGroup(marsOrbitTimeMs);
@@ -215,7 +198,7 @@ public class ProjectSpace extends Applet {
         root.addChild(tgMarsOrbit);
 
         //Jupiter
-        tr.setTranslation(new Vector3f(140f,.0f,.0f)); //distance to center
+        tr.setTranslation(new Vector3f(140f,0f,0f)); //distance to center
         TransformGroup tgJupiter = new TransformGroup(tr);//-.2f,.15f,.15f
         TransformGroup tgJupiterRotat =createRotationTransformGroup(jupiterRotationTimeMs);//rotation
         TransformGroup tgJupiterOrbit = createOrbitTransformGroup(jupiterOrbitTimeMs);
@@ -225,7 +208,7 @@ public class ProjectSpace extends Applet {
         root.addChild(tgJupiterOrbit);
 
         //Saturn
-        tr.setTranslation(new Vector3f(195f,.0f,.0f)); //distance to center
+        tr.setTranslation(new Vector3f(195f,0f,0f)); //distance to center
         TransformGroup tgSaturn = new TransformGroup(tr);//-.2f,.15f,.15f
         TransformGroup tgSaturnRotat =createRotationTransformGroup(saturnRotationTimeMs);//rotation
         TransformGroup tgSaturnOrbit = createOrbitTransformGroup(saturnOrbitTimeMs);
@@ -235,7 +218,7 @@ public class ProjectSpace extends Applet {
         root.addChild(tgSaturnOrbit);
 
         //Uranus
-        tr.setTranslation(new Vector3f(230f,.0f,.0f)); //distance to center
+        tr.setTranslation(new Vector3f(230f,0f,0f)); //distance to center
         TransformGroup tgUranus = new TransformGroup(tr);//-.2f,.15f,.15f
         TransformGroup tgUranusRotat =createRotationTransformGroup(uranusRotationTimeMs);//rotation
         TransformGroup tgUranusOrbit = createOrbitTransformGroup(uranusOrbitTimeMs);
@@ -245,7 +228,7 @@ public class ProjectSpace extends Applet {
         root.addChild(tgUranusOrbit);
 
         //Neptune
-        tr.setTranslation(new Vector3f(250f,.0f,.0f)); //distance to center
+        tr.setTranslation(new Vector3f(250f,0f,0f)); //distance to center
         TransformGroup tgNeptune = new TransformGroup(tr);//-.2f,.15f,.15f
         TransformGroup tgNeptuneRotat =createRotationTransformGroup(neptuneRotationTimeMs);//rotation
         TransformGroup tgNeptuneOrbit = createOrbitTransformGroup(neptuneOrbitTimeMs);
@@ -255,7 +238,7 @@ public class ProjectSpace extends Applet {
         root.addChild(tgNeptuneOrbit);
 
         //Pluto
-        tr.setTranslation(new Vector3f(265,.0f,.0f)); //distance to center
+        tr.setTranslation(new Vector3f(265,0f,0f)); //distance to center
         TransformGroup tgPluto = new TransformGroup(tr);//-.2f,.15f,.15f
         TransformGroup tgPlutoRotat =createRotationTransformGroup(plutoRotationTimeMs);//rotation
         TransformGroup tgPlutoOrbit = createOrbitTransformGroup(plutoOrbitTimesMs);
@@ -265,85 +248,6 @@ public class ProjectSpace extends Applet {
         root.addChild(tgPlutoOrbit);
 
 
-
-
-
-        /*tgSolar.addChild(tgMercuryRotat);//review this. should it be tgMercury
-        tgMercuryRotat.addChild(tgMercury);// should rotat be after tgmercury
-        tgMercury.addChild(mercury);*/
-
-       /* tr.setScale(.4);
-        tr.setIdentity();
-        tr.setTranslation(new Vector3f(-.3f,.35f,.2f));
-        TransformGroup tgVenus = new TransformGroup(tr); //-.3f,.35f,.2f
-        TransformGroup tgVenusRotat =createRotationTransformGroup(venusRotationTimeMs, true);//rotation
-        tgSolar.addChild(tgVenusRotat);
-        tgVenusRotat.addChild(tgVenus);
-        tgVenus.addChild(venus);
-
-        TransformGroup tgMoon = new TransformGroup(tr);
-
-        tr.setScale(.45);
-        tr.setIdentity();
-        tr.setTranslation(new Vector3f(.45f,.57f,.45f));
-        TransformGroup tgEarth = new TransformGroup(tr); //.45f,.57f,.45f
-        TransformGroup tgEarthRotat =createRotationTransformGroup(earthRotationTimeMs, true);//rotation
-        tgSolar.addChild(tgEarthRotat);
-        tgEarthRotat.addChild(tgEarth);
-        tgEarth.addChild(earth);
-
-
-        tr.setScale(.225);
-        tr.setIdentity();
-        tr.setTranslation(new Vector3f(.55f,.4f,.6f));
-        TransformGroup tgMars = new TransformGroup(tr);//.55f,.4f,.6f
-        TransformGroup tgMarsRotat =createRotationTransformGroup(marsRotationTimeMs, true);//rotation
-        tgSolar.addChild(tgMarsRotat);
-        tgMarsRotat.addChild(tgMars);
-        tgMars.addChild(mars);
-
-        tr.setScale(1.3);
-        tr.setIdentity();
-        tr.setTranslation(new Vector3f(.8f,-.8f,-.45f));
-        TransformGroup tgJupiter = new TransformGroup(tr); //.8f,-.8f,-.45f
-        TransformGroup tgJupRotat =createRotationTransformGroup(jupiterRotationTimeMs, true);//rotation
-        tgSolar.addChild(tgJupRotat);
-        tgJupRotat.addChild(tgJupiter);
-        tgJupiter.addChild(jupiter);
-
-        tr.setScale(1.2);
-        tr.setIdentity();
-        tr.setTranslation(new Vector3f(0.89f,-0.9f,-0.45f));
-        TransformGroup tgSaturn = new TransformGroup(tr); //.89f,-.9f,.-45f
-        TransformGroup tgSatRotat =createRotationTransformGroup(saturnRotationTimeMs, true);//rotation
-        tgSolar.addChild(tgSatRotat);
-        tgSatRotat.addChild(tgSaturn);
-        tgSaturn.addChild(saturn);
-
-        tr.setScale(1.1);
-        tr.setIdentity();
-        tr.setTranslation(new Vector3f(1.5f,1f,1f));
-        TransformGroup tgUranus = new TransformGroup(tr);//1.5f,1f,1f
-        TransformGroup tgUranRotat =createRotationTransformGroup(uranusRotationTimeMs, true);//rotation
-        tgSolar.addChild(tgUranRotat);
-        tgUranRotat.addChild(tgUranus);
-        tgUranus.addChild(uranus);
-        tr.setScale(1.);
-        tr.setIdentity();
-        tr.setTranslation(new Vector3f(01f,0f,0f));
-        TransformGroup tgNeptune = new TransformGroup(tr);//1f,0f,0f
-        TransformGroup tgNepRotat =createRotationTransformGroup(neptuneRotationTimeMs, true);//rotation
-        tgSolar.addChild(tgNepRotat);
-        tgNepRotat.addChild(tgNeptune);
-        tgNeptune.addChild(neptune);
-
-        tr.setScale(.2);
-        tr.setIdentity();
-        tr.setTranslation(new Vector3f(.9f,1.5f,1.5f));
-        TransformGroup tgPluto = new TransformGroup(tr);//.9f,1.5f,1.5f
-        tgSolar.addChild(tgPluto);
-        tgPluto.addChild(pluto); */
-
         //light for the sun
         BoundingSphere bounds = new BoundingSphere(new Point3d(0,0,0),Double.MAX_VALUE);
         Background background = new Background(0.0f, 0.0f, 0.0f);
@@ -351,9 +255,10 @@ public class ProjectSpace extends Applet {
         root.addChild(background);
 
         PointLight light = new PointLight();//please check over this
-        light.setColor(new Color3f(Color.white));
-        light.setAttenuation(10, 0, 0);
-        light.setPosition(0f,.0f,.0f);
+        light.setColor(new Color3f(1.0f,0.898f,0.8f));
+        light.setCollidable(true);
+        light.setAttenuation(2.5f, 0.0f, 0.0f);
+        light.setPosition(0f,0f,0f);
         light.setInfluencingBounds(bounds);
         root.addChild(light);
 
